91fdee7135514badd35aff9fe8d9e43d36f42c78
ziliang2xiaoxiao <zliang1@andrew.cmu.edu>
2018-10-30 14:10:31 -0700

    add hasHighFrequency method, but the function now is not completed

--- SoundChart/app/src/main/java/com/door/soundchart/AudioProcess.java
+++ SoundChart/app/src/main/java/com/door/soundchart/AudioProcess.java
@@ -19,9 +19,11 @@ public class AudioProcess {
     private ArrayList<int[]> outBuf = new ArrayList<int[]>(); // processed data
     private boolean isRecording = false;
     private int length = 1024;
-    private int half_len = 512;
+    private int halfLen = 512;
     private int batchNum = 50;
     private int sampleRate = 44100;
+    private int minHighFreq = 18000;
+    private int highFreqIdx = (int)(minHighFreq * 2.0 / sampleRate * 512);
 
     Context mContext;
     private int shift = 30;
@@ -29,6 +31,7 @@ public class AudioProcess {
 
     //启动程序
     public void start() {
+        Log.d("minHighFreq", String.valueOf(minHighFreq));
         isRecording = true;
         new RecordThread().start();
         new DrawThread().start();
@@ -38,6 +41,19 @@ public class AudioProcess {
     public void stop() {
         isRecording = false;
         inBuf.clear();
+    }
+
+    public void hasHighFreq(int[] buf) {
+        int max = 0;
+        double average = 0;
+        for (int i = highFreqIdx; i < halfLen; i++) {
+            if (buf[i] > max) {
+                max = buf[i];
+                average += buf[i] *1.0 / (halfLen - highFreqIdx);
+            }
+        }
+        Log.d("MAX", String.valueOf(max));
+        Log.d("Avg", String.valueOf(average));
     }
 
     //录音线程
@@ -62,7 +78,7 @@ public class AudioProcess {
                 audioRecord.startRecording();
                 while (isRecording) {
                     // fft batch
-                    int[] power = new int[half_len];
+                    int[] power = new int[halfLen];
                     boolean isDataReady = false;
                     for (int i = 0; i < batchNum; i++) {
                         resultOfRead = audioRecord.read(buffer, 0, length); // read blocking
@@ -83,8 +99,8 @@ public class AudioProcess {
                             x[j] = (double) tmpBuf[j];
                         }
                         convert.fft(x, y);
-                        for (int j = 0; j < half_len; j++) {
-                            power[j] += x[j + half_len] * x[j + half_len] + y[j + half_len] * y[j + half_len];
+                        for (int j = 0; j < halfLen; j++) {
+                            power[j] += x[j + halfLen] * x[j + halfLen] + y[j + halfLen] * y[j + halfLen];
                         }
 
                         if (isDataReady) {
@@ -129,11 +145,14 @@ public class AudioProcess {
                 for(int i = 0; i < buf.size(); i++){
                     int[]tmpBuf = buf.get(i);
                     Log.d("OUTPUT", Arrays.toString(tmpBuf));
+                    hasHighFreq(tmpBuf);
                     // TODO: SimpleDraw tmpBuf
                 }
             }
         }
     }
+
+
 
 }
 
