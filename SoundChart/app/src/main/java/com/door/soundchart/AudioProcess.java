31964af006ae8767988315ddebbb3b3e0a278b33
tttlw1778 <tttlw1778@gmail.com>
2018-11-13 16:44:39 -0800

    delete useless things

--- SoundChart/app/src/main/java/com/door/soundchart/AudioProcess.java
+++ SoundChart/app/src/main/java/com/door/soundchart/AudioProcess.java
@@ -1,11 +1,9 @@
 package com.door.soundchart;
 
-<<<<<<< HEAD
-=======
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
->>>>>>> 9d3991f0d8a1f3312b9724a482da1c5c623422ec
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
@@ -71,7 +69,7 @@ public class AudioProcess {
         }
 
         if (cnt > 50) {
-<<<<<<< HEAD
+
             Message msg = new Message();
             msg.what = DETECT_ULTRASOUND;
             msg.obj = "DETECT SUCCESSFULLY";
@@ -86,17 +84,6 @@ public class AudioProcess {
 
 
 
-    //录音线程
-=======
-            Log.e("DETECT", "High Freq Detected=======================");
-            // TODO: do something after any ultrasound has been detected
-            Intent service_intent = new Intent(, NoiseJammingService.class);
-            service_intent.putExtra("is_blocked", false);
-            context.startService(service_intent);
-        }
-    }
-
->>>>>>> 9d3991f0d8a1f3312b9724a482da1c5c623422ec
     class RecordThread extends Thread {
         private AudioRecord audioRecord;
         private FFT convert;
